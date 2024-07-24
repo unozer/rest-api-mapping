6a6ba39fa5fa5df90bf55b9ffb02c749430b7e11
unozer <40232537+unozer@users.noreply.github.com>
2024-07-24 15:23:34 +0200

    first commit

---
+++ src/main/java/it/gp/rest_api_mapping/RestApiMappingApplication.java
@@ -0,0 +1,13 @@
+package it.gp.rest_api_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gp/rest_api_mapping/RestApiMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gp.rest_api_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
